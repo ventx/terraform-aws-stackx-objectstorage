14f0eeff733f09e73085b2cfe584a2cde3e2efdf
hansjoergventx <hans-joerg@ventx.de>
2022-09-08 20:47:17 +0200

    chore: test template diagram as code from stackx-cluster

--- docs/images/diagram.py
+++ docs/images/diagram.py
@@ -1,21 +1,28 @@
+from urllib.request import urlretrieve
+
 from diagrams import Cluster, Diagram
 from diagrams.aws.compute import ElasticKubernetesService
 from diagrams.aws.security import KeyManagementService
 from diagrams.aws.security import IdentityAndAccessManagementIam
 from diagrams.aws.security import IdentityAndAccessManagementIamRole
 from diagrams.aws.management import Cloudwatch
+from diagrams.aws.compute import Fargate
+from diagrams.k8s.controlplane import KubeProxy
+from diagrams.custom import Custom
 
+vpccni_url = "https://user-images.githubusercontent.com/50456/83688420-3a52a080-a5a2-11ea-8ebd-da3813717d37.png"
+vpccni_icon = "icon-vpccni.png"
+urlretrieve(vpccni_url, vpccni_icon)
 
-with Diagram("terraform-aws-stackx-objectstorage", outformat="png", filename="screenshot1", show=False):
+with Diagram("terraform-aws-stackx-cluster", outformat="png", filename="screenshot1", show=False):
     with Cluster("EKS"):
 
       eks = ElasticKubernetesService("EKS")
       kms = KeyManagementService("Secrets Encryption")
-      iam = IdentityAndAccessManagementIam("IAM")
-
-      iam_group = [
-        IdentityAndAccessManagementIam("IAM") >> IdentityAndAccessManagementIam("OIDC Provider"),
-        IdentityAndAccessManagementIamRole("Cluster Role")
-      ]
+      cr = IdentityAndAccessManagementIamRole("Cluster Role")
+      oidc = IdentityAndAccessManagementIam("OIDC Provider")
+      fg = Fargate("Fargate Profile")
       cloudwatch = Cloudwatch("Control-Plane Logs")
-      eks >> [kms, cloudwatch, iam_group]
+      cni = Custom("AWS VPC", vpccni_icon)
+      proxy = KubeProxy("Kube Proxy")
+      eks >> [kms, cloudwatch, oidc, fg]
